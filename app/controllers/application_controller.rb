479f38e419763fc4d113e350421a6a4158f61760
vinicius zamprogno <vinicius.quimica@gmail.com>
2025-03-13 16:28:40 -0300

    Pundit, cloudinary setados e migrate de users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # include Pundit::Authorization
+
+  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:photo, :first_name, :last_name, :user_name, :gender, :birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :first_name, :last_name, :user_name, :gender, :birthday])
+  end
+
+  # private
+
+  # def skip_pundit?
+  #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  # end
 end
---
+++ db/migrate/20250313180652_add_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :user_name, :string
+    add_column :users, :gender, :string
+    add_column :users, :birthday, :date
+  end
+end
