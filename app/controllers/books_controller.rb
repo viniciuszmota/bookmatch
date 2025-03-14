34875c060de4c115584369ab73036e3fec7262f6
vinicius zamprogno <vinicius.quimica@gmail.com>
2025-03-14 13:36:06 -0300

    show book e editi book add, sem genre

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,28 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: %i[show edit update]
 
   def index
     @books = Book.all
   end
 
-  
+  def show
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "Livro atualizado com sucesso."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :publisher, :condition, :genre_id)
+  end
 end
