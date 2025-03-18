af208775e02160860cb75254c3f56c807b3de925
vinicius zamprogno <vinicius.quimica@gmail.com>
2025-03-18 12:25:44 -0300

    cadastro atualizado

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,12 @@ class User < ApplicationRecord
   has_many :books
   has_many :sent_messages, class_name: "Message", foreign_key: "sender_id", dependent: :destroy
   has_many :received_messages, class_name: "Message", foreign_key: "receiver_id", dependent: :destroy
-  belongs_to :sender, class_name: "User"
-  belongs_to :receiver, class_name: "User"
+  # belongs_to :sender, class_name: "User"
+  # belongs_to :receiver, class_name: "User"
+
+  belongs_to :sender, class_name: "User", optional: true
+  belongs_to :receiver, class_name: "User", optional: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
