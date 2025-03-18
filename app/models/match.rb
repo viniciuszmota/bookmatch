06f4117f56a0841cc35b27282cf43017130d0a88
vinicius zamprogno <vinicius.quimica@gmail.com>
2025-03-18 15:39:49 -0300

    botão de chat implementado

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,6 @@
 class Match < ApplicationRecord
+  belongs_to :first_like, class_name: "Like", foreign_key: "first_like_id"
+  belongs_to :second_like, class_name: "Like", foreign_key: "second_like_id"
 
   has_many :messages, dependent: :destroy
 end
-
- 
