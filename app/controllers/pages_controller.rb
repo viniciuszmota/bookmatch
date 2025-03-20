b56306b461ec7174c8c9d9c3123620c73e81902a
Jorge Vieira <jorvieira88@gmail.com>
2025-03-20 17:40:08 -0300

    front chat criado

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,18 +6,15 @@ class PagesController < ApplicationController
 
   def matchmaking
     @book = Book
-            .left_joins(:likes) # Faz um LEFT JOIN com a tabela de likes
-            .where.not(user_id: current_user.id) # Evita que o usuário veja seus próprios livros
-            .where("likes.user_id IS NULL OR likes.user_id != ?", current_user.id).first
+            .where.not(user_id: current_user.id)
+            .where.not(
+              id: Like.where(user_id: current_user.id).select(:book_id)
+            )
+            .first
     @user = current_user
-    # Armazena na sessão apenas os livros que não pertencem ao usuário logado
-    
-    # Pega o próximo livro do array (se existir)
-
-      # Se não houver mais livros, reinicia o array sem os livros do usuário
-  end
-    
+  
+  end
+    
 end
 
-
-
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "books#index"
+  root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :books
   resources :likes
