6b428c9df7f3d89f59128bd958df82614258c983
Marcio Vianna <mvianna.1994@gmail.com>
2025-03-21 12:26:58 -0300

    Index de books estilizado

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,7 +10,6 @@ class LikesController < ApplicationController
     book_owner_id = @like.book.user_id
     current_user_id = current_user.id
     second_like = Like.find_by(user_id: book_owner_id, book_id: current_user.books.pluck(:id))
-    
     @like.save
     if second_like
       if second_like.liked_action
